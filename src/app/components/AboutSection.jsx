c1530169f67dbd4d33880f479fb17f4dfd95279b
Krishan Kumar <92982687+Crimson-03@users.noreply.github.com>
2023-09-22 18:28:19 +0530

    Update AboutSection.jsx
    `'` can be escaped with `&apos;`

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -66,8 +66,8 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I'm a full stack developer proficient in HTML, React, CSS, and
-            JavaScript for the front end, and Node.js for the back end. I'm
+            I&apos;m a full-stack developer proficient in HTML, React, CSS, and
+            JavaScript for the front end, and Node.js for the back end. I&apos;m
             dedicated to building web applications that combine sleek user
             interfaces with robust server-side functionality.
           </p>
